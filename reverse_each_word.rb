0c3738147d54bcce4cdc198da317ef24419984f9
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 23:29:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+# def reverse_each_word(string)
+#   new_words = []
+#   words = string.split(' ')
+#   words.each { |word| new_words.push(word.reverse) }
+#   new_words.join(' ')
+# end
+
+def reverse_each_word(string)
+  words = string.split(' ')
+  words.collect { |word| word = word.reverse}.join(' ')
+end
